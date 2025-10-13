e328785dd15a0ed3bd99323c785de321d7108083
HuJun017 <166590848+HuJun017@users.noreply.github.com>
2025-10-13 13:52:31 +0000

    routes

--- mini-blog/src/app/app.routes.ts
+++ mini-blog/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { PostList } from './components/post-list/post-list';
+import { PostDetailComponent } from './components/post-detail/post-detail';
+import { NewPostFormComponent } from './components/new-post-form/new-post-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/posts', pathMatch: 'full' },
+  { path: 'posts', component: PostListComponent },
+  { path: 'post/:id', component: PostDetailComponent },
+  { path: 'new-post', component: NewPostFormComponent },
+  { path: 'edit-post/:id', component: NewPostFormComponent },
+  { path: '**', redirectTo: '/posts' }
+];
